5b9ce2943bdd243933db4b3acaa0c963f03c5bd7
nadien <sergiomen4@gmail.com>
2015-05-14 12:24:49 -0500

    changes

--- functions.js
+++ functions.js
@@ -9,39 +9,38 @@ function displayBlock(){
 		old.innerHTML = "I have +18";
 		teen.innerHTML = "I have -18";
 	
-	$("body").append(old, teen);
+	$("#Show-page").append(old, teen);
 	
 	//+18
 	$("h1:first").click(function(){
 		$.cookie('cookie_name', 'cookie_value', { expires: 2, path: '/', domain: 'yourwebsite.com', secure: true });
-		$("body").hide("slow");
+		$("#Show-page").hide(2000);
 	});
 	
 	//-18
 	$("h1:eq(1)").click( 
 		function(){
-		/*Npermission = document.createElement("p");
-		Npermission.innerHTML = "You don't have permission to be here, go study";	
-		$("body").append(Npermission);*/
 		window.location.href = 'http://xxxsexy.cf';
-	}
-		
+		}
 	);
-	
 	});
 			
 }
-if( $("document")){
+
 	
-}
 //load class css
-$("document").ready( function(){
+/*$("document").ready( function(){
 		$("body").addClass("estilachos");
 		
 	 }
 	); 
+	*/
 	
-	
+	//When the page is ready, 
+$("document").ready(function(){
+		$("#Show-page").addClass("show").addClass("styleBody");
+}	
+);
 
 onload = displayBlock;
 
